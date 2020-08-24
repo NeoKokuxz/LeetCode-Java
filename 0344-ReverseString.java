455b58f8612bfa620579e86d821d447d42aa8c41
Neo Chen <43288434+NeoKokuxz@users.noreply.github.com>
2020-08-23 20:59:56 -0400

    Update 0344-ReverseString.java

--- 0344-ReverseString.java
+++ 0344-ReverseString.java
@@ -1,9 +1,14 @@
 public class ReverseString {
-    public static void main(String[] args) {
-
-    }
-
     public static void reverseString(char[] chars){
-        
+        int start = 0;
+        int end = s.length-1;
+
+        while (start < end){
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
