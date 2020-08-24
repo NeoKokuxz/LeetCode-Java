c0201b427eca545246eaa0d6be9f717ba02f1406
Neo Chen <43288434+NeoKokuxz@users.noreply.github.com>
2020-08-24 14:30:09 -0400

    Create 0104-MaximumDepthOfBinaryTree.java

---
+++ 0104-MaximumDepthOfBinaryTree.java
@@ -0,0 +1,24 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class MaxDepthBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int maxLeft = maxDepth(root.left);
+        int maxRight = maxDepth(root.right);
+        return Math.max(maxLeft, maxRight) + 1;
+    }
+}
+
