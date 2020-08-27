d8bc5c64c03402c07513c8f8f49fb594cd3d51d1
Neo Chen <43288434+NeoKokuxz@users.noreply.github.com>
2020-08-27 17:46:43 -0400

    Create 0171-ExcelSheetColumnNumber.java

---
+++ 0171-ExcelSheetColumnNumber.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int titleToNumber(String s) {
+        int result = 0;
+        for(char c : s.toCharArray()){
+            //C - 'A' turns from ascii to A = 1, B = 2
+            int i = c - 'A' + 1;
+            // Example:
+            // AA
+            // First A:
+            // A - A + 1 = 1 - 1 + 1 = 1
+            // result = 0*26 + 1 = 1
+            // Second A:
+            // A - A + 1 = 1
+            // result = result(1) * 26 + 1 = 27
+            // So 27 = AA
+            result = result*26 + i;
+        }
+        return result;
+    }
+}
