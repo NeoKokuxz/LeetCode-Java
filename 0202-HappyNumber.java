ef3ff7d0662dc26b74ec28cec66c967ba06ffc0e
Neo Chen <43288434+NeoKokuxz@users.noreply.github.com>
2020-08-28 22:48:02 -0400

    Create 0202-HappyNumber.java

---
+++ 0202-HappyNumber.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isHappy(int n) {
+        if(n == 1){
+            return true;
+        }
+        int last, current = n;
+        int happy = 0;
+
+        //This loop will finish current number
+        while (current > 0){
+            //Get the last digit of N
+            last = current % 10; 
+            happy += last * last;
+            
+            //Get remaining number of N
+            //current at this point will eventually end up 0 to finish current happy number
+            current = current / 10;
+        }
+        
+        //if the happy from the previous result is 1 then it's happy
+        if (happy == 1) {
+            return true; // It is happy
+        } else if (happy == 4){
+            return false;
+        }
+        
+        // 19 will turn into 1^2 + 9^2 = 82, and 82 will be pass into recursive
+        return isHappy(happy);
+    }
+}
