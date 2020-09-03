5cdf0e71d3e5f44d80f21da9c9c78969fe477e96
Neo Chen <43288434+NeoKokuxz@users.noreply.github.com>
2020-09-02 21:09:09 -0400

    Update 0172-FactorialTrailingZeros.java

--- 0172-FactorialTrailingZeros.java
+++ 0172-FactorialTrailingZeros.java
@@ -1,10 +1,17 @@
 class Solution {
     public int trailingZeroes(int n) {
+        //Count how many 5 in n
         int count = 0;
+        
+        //If n is greather or equal to 5
         while (n >= 5){
+            //Add the all the 5 in n to count
             count += n/5;
+            // Example: N = 25, and n/5 + count = 5 -> count
+            // Now N/5 = 25/5 = 5 now
             n /= 5;
         }
+        
         return count;
     }
 }
